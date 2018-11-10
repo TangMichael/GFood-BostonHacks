b5a99c6547da35bcadc4b5ede636ae0807b962c8
Tristan <phistan007@gmail.com>
2018-11-10 17:36:09 -0500

    backend receives searched word in controller

---
+++ GFood_API/Controllers/ClarifaiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GFood_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClarifaiController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET api/values/5
+        [HttpPost]
+        public ActionResult<string> Get([FromBody]string search)
+        {
+        
+            return search;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
