9a4c99eb5535feccc61253fc79871979fd62566e
Tristan <phistan007@gmail.com>
2018-11-10 21:22:57 -0500

    Algolia services added

---
+++ GFood_API/AlgoliaService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GFood_API
+{
+    public class AlgoliaService
+    {
+        public void GetPictureLocation(string picture_url)
+        {
+
+        }
+    }
+}
