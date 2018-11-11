d1038a101af324b23d828875027ebe3fd3cacf3d
Kevin Luu <kevinateur@hotmail.com>
2018-11-11 00:42:10 -0500

    Reverse engineer

--- GFood_API/Services/AlgoliaService.cs
+++ GFood_API/Services/AlgoliaService.cs
@@ -67,5 +67,6 @@ namespace GFood_API
         }
 
 
+
     }
     }
--- GFood_API/Services/ClarifaiService.cs
+++ GFood_API/Services/ClarifaiService.cs
@@ -9,13 +9,17 @@ using Newtonsoft.Json.Linq;
 
 namespace GFood_API
 {
-    public class ClarifaiHandler
+    public class ClarifaiService
     {
         ClarifaiClient client;
+        private const string API_KEY = "dfa26cc77ace4f539eff32c420226fbc";
 
+        public ClarifaiService() {
+
+        }
 
         public async Task<ClarifaiResponse<SearchInputsResult>> SearchByConcept(string concept) {
-            ClarifaiClient client = new ClarifaiClient("dfa26cc77ace4f539eff32c420226fbc");
+            ClarifaiClient client = new ClarifaiClient(API_KEY);
             var response = await client.SearchInputs(SearchBy.ConceptName("breakfast"))
             .Page(1)
             .ExecuteAsync();
@@ -29,6 +33,10 @@ namespace GFood_API
                 urlList.Add(url);
             }
             return urlList;
+        }
+
+        public void UrlWithAddress(List<string> urlList, JObject jObject) {
+
         }
 
     }
