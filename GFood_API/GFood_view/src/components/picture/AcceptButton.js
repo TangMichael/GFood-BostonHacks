17c75fa1d162356d35aa8c1bc7e766a7859f16be
Kevin Luu <kevinateur@hotmail.com>
2018-11-11 06:24:52 -0500

    Change buttons

--- GFood_API/GFood_view/src/components/picture/AcceptButton.js
+++ GFood_API/GFood_view/src/components/picture/AcceptButton.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react';
+import Icon from "semantic-ui-react/dist/commonjs/elements/Icon/Icon";
 
 export const AcceptButton = props => {
     return (
       <div>
-        <Button onClick={props.onAccept}>Accept</Button>
+          <Button onClick={props.onAccept} as='div' labelPosition='right'>
+              <Button color='red'>
+                  <Icon name='heart' />
+                  Like
+              </Button>
+          </Button>
       </div>
     )
 }
--- GFood_API/GFood_view/src/components/picture/RejectButton.js
+++ GFood_API/GFood_view/src/components/picture/RejectButton.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react';
+import Icon from "semantic-ui-react/dist/commonjs/elements/Icon/Icon";
 
 export const RejectButton = props => {
     return (
       <div>
-        <Button onClick={props.onReject}>Reject</Button>
+          <Button onClick={props.onReject} as='div' labelPosition='right'>
+              <Button color='green'>
+                  <Icon name='thumbs down outline' />
+                  Next
+              </Button>
+          </Button>
       </div>
     )
 }
